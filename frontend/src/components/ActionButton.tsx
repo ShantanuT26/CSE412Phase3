1ed30d956fe10763ba4caab1f2cc4abf228cb174
ashcodex505 <ashishkurse@gmail.com>
2025-05-02 19:52:58 -0700

    first

--- frontend/src/components/ActionButton.tsx
+++ frontend/src/components/ActionButton.tsx
@@ -15,7 +15,7 @@ export const ActionButton: React.FC<ActionButtonProps> = ({
   return (
     <Button
       className={cn(
-        "bg-white text-[32px] text-[rgb(56,56,56)] font-light w-full py-6 px-[70px] rounded-[31px] h-auto hover:bg-gray-50 transition-colors",
+        "bg-white text-[32px] text-[rgb(238, 0, 0)] font-light w-full py-6 px-[70px] rounded-[31px] h-auto hover:bg-gray-50 transition-colors",
         "max-md:max-w-full max-md:px-5",
         className,
       )}
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -59,7 +59,7 @@ export const Welcome: React.FC = () => {
 
   
   return (
-    <main className="bg-[rgba(239,239,239,1)] min-h-screen flex flex-col overflow-hidden items-stretch pt-16 pb-[401px] px-[78px] max-md:pb-[100px] max-md:px-5">
+    <main className="bg-[rgb(255, 6, 6)] min-h-screen flex flex-col overflow-hidden items-stretch pt-16 pb-[401px] px-[78px] max-md:pb-[100px] max-md:px-5">
       <section className="max-md:max-w-full">
         <div className="gap-5 flex max-md:flex-col max-md:items-stretch">
           <div className="w-6/12 max-md:w-full max-md:ml-0">
